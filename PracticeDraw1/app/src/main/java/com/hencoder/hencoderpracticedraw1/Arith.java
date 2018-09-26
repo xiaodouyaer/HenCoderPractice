6176e2d14ecff44ee37171b41d33a9c77dac0712
liuchaoya <1090969255@qq.com>
2018-09-26 16:00:36 +0800

    第一套练习补充

---
+++ PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/Arith.java
@@ -0,0 +1,71 @@
+package com.hencoder.hencoderpracticedraw1;
+
+import android.support.annotation.IntRange;
+
+import java.math.BigDecimal;
+
+/**
+ * @classDescription: 描述：
+ * @author: LiuChaoya
+ * @createTime: 2018/9/21 0021 9:57.
+ * @email: 1090969255@qq.com
+ */
+public class Arith {
+    /**
+     * 提供精确加法计算的add方法
+     *
+     * @param value1 被加数
+     * @param value2 加数
+     * @return 两个参数的和
+     */
+    public static double add(double value1, double value2) {
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.add(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确减法运算的sub方法
+     *
+     * @param value1 被减数
+     * @param value2 减数
+     * @return 两个参数的差
+     */
+    public static double sub(double value1, double value2) {
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.subtract(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确乘法运算的mul方法
+     *
+     * @param value1 被乘数
+     * @param value2 乘数
+     * @return 两个参数的积
+     */
+    public static double mul(double value1, double value2) {
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.multiply(b2).doubleValue();
+    }
+
+
+    public static double div(double value1, double value2) {
+        return div(value1,value2,4);
+    }
+    /**
+     * 提供精确的除法运算方法div
+     *
+     * @param value1 被除数
+     * @param value2 除数
+     * @param scale  精确范围
+     * @return 两个参数的商
+     */
+    public static double div(double value1, double value2, @IntRange(from = 1,to = 9) int scale) {
+        BigDecimal b1 = new BigDecimal(Double.toString(value1));
+        BigDecimal b2 = new BigDecimal(Double.toString(value2));
+        //默认保留两位会有错误，这里设置保留小数点后4位
+        return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+}
--- PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -8,30 +8,36 @@ import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw1.Arith;
 
 public class Practice11PieChartView extends View {
 
+    private final String TAG = getClass().getSimpleName();
     private int viewWidth;
     private int viewHeight;
     private Paint paint;
     private Path path;
     private Data[] titles;
     private RectF rectF;
+    private int radius;
 
     {
         titles = new Data[]{
-                new Data("Froyo", "#00ff00", 0.01),
-                new Data("Gingerbread", "#8000FF", 0.05),
-                new Data("Ice Cream Sandwich", "#C0C0C0", 0.05),
-                new Data("Jelly Bean", "#008000", 0.15),
-                new Data("KitKat", "#0000ff", 0.28),
-                new Data("Lollipop", "#ff0000", 0.14),
-                new Data("Marshmallow", "#ffff00", 0.32),
+                new Data("Marshmallow", "#F9C100", 50),
+                new Data("Froyo", "#00ff00", 4),
+                new Data("Gingerbread", "#9726B3", 8),
+                new Data("Ice Cream Sandwich", "#9E9E9E", 8),
+                new Data("Jelly Bean", "#309688", 60),
+                new Data("KitKat", "#4096F7", 100),
+                new Data("Lollipop", "#EA422D", 130),
         };
         rectF = new RectF();
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         path = new Path();
+        radius = 200;
     }
 
     public Practice11PieChartView(Context context) {
@@ -58,22 +64,69 @@ public class Practice11PieChartView extends View {
         int cX = viewWidth >> 1;
         int cY = viewHeight >> 1;
 
-        rectF.set(cX - 200, cY - 200, cX + 200, cY + 200);
+        rectF.set(cX - radius, cY - radius, cX + radius, cY + radius);
 
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawArc(rectF, 0, 360, true, paint);
+
+        int startAngle = -titles[0].angle;
+        for (Data data : titles) {
+            paint.setColor(Color.parseColor(data.color));
+            paint.setStyle(Paint.Style.FILL);
+            if ("Lollipop".equals(data.title)) {
+                double centerRadian = Math.PI * (startAngle + ((data.angle - 2) >> 1) - 180) / 180;//中心弧度
+                double offsetX = 10;
+                double offsetY = (int) (offsetX * Math.tan(centerRadian));
+                rectF.offset(-(float) offsetX, -(float) offsetY);
+                canvas.drawArc(rectF, startAngle, data.angle - 2, true, paint);
+                startAngle += data.angle;
+
+                double hypotenuse = Arith.div(offsetX, Math.cos(centerRadian), 9);
+                double scale = Arith.div(hypotenuse, Arith.add(hypotenuse, radius), 9);
+                double radianX = cX - Arith.div(offsetX, scale);
+                double radianY = cY - Arith.div(offsetY, scale);
+
+                paint.setColor(Color.WHITE);
+                paint.setStyle(Paint.Style.STROKE);
+                path.moveTo((float) (radianX - 3), (float) (radianY - 3));
+                path.lineTo((float) (radianX - offsetX), (float) (radianY - offsetY));
+                path.lineTo((float) (radianX - offsetX - 100), (float) (radianY - offsetY));
+                canvas.drawPath(path, paint);
+
+                paint.setTextSize(16);
+                float measureText = paint.measureText(data.title);
+                canvas.drawText(data.title, (float) (radianX - offsetX - 100 - measureText - 20), (float) (radianY - offsetY), paint);
+
+
+                //测试一下drawText的x，y到底是什么坐标
+                String test = "这是个测试位置的字符串";
+                float testLen = paint.measureText(test);
+                paint.setTextSize(30);
+                Paint.FontMetrics fontMetrics0 = paint.getFontMetrics();
+                Log.d(TAG, String.format(
+                        "Paint.FontMetrics0{%f，%f，%f，%f，%f}",
+                        fontMetrics0.top,
+                        fontMetrics0.ascent,
+                        fontMetrics0.descent,
+                        fontMetrics0.bottom,
+                        fontMetrics0.leading
+                ));
+                canvas.drawText(test, 0, 0, paint);
+                canvas.drawText(test, 0, 100, paint);
+                continue;
+            }
+            canvas.drawArc(rectF, startAngle, data.angle - 2, true, paint);
+            startAngle += data.angle;
+        }
     }
 
     private class Data {
         private String title;
         private String color;
-        private double percent;
+        private int angle;
 
-        public Data(String title, String color, double percent) {
+        public Data(String title, String color, int angle) {
             this.title = title;
             this.color = color;
-            this.percent = percent;
+            this.angle = angle;
         }
     }
 }
